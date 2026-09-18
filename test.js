aeabed8818f2da4fd9ba1723a3e662b8c96e1902
kmagesh197-cloud <kmagesh197-cloud@users.noreply.github.com>
2026-09-19 02:17:30 +0530

    Add support for export prefix in env files

--- test.js
+++ test.js
@@ -4,11 +4,14 @@ const input = `
 # Database config
 DB_HOST=localhost
 DB_PORT=5432
-DB_NAME=mydb
+export DB_NAME=mydb
+export API_KEY=secret123
 `;
 
 const result = parseEnv(input);
 console.log(result);
 console.assert(result.DB_HOST === 'localhost');
 console.assert(result.DB_PORT === '5432');
+console.assert(result.DB_NAME === 'mydb');
+console.assert(result.API_KEY === 'secret123');
 console.log('All tests passed');
